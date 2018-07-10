9dc2e2aed0eabc36eced7a924bf6578f3dea7c2d
Brad Schrag <brad.schrag@gmail.com>
2018-07-10 14:59:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
 
+function nestedTarget() {
+  return document.querySelector('div#nested div.target')
+}
+
+function deepestChild() {
+  return document.querySelectorAll('dev#grand-node div')
+}
